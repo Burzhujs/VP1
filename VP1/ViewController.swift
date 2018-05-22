e0a2c5c25fdb71af18b188b7ffb1e827f748b0fc
Matiss Mamedovs <matiss.mamedovs@di.lv>
2018-05-22 16:45:34 +0300

    first

---
+++ VP1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VP1
+//
+//  Created by Matiss on 22/05/2018.
+//  Copyright © 2018 Matiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
